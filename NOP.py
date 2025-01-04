caabdc8cf2044e7b7efc69203c6cd12c93504f72
barishaxxer <bariscankaxxer@gmail.com>
2025-01-04 22:53:03 +0300

    Feat: Bind shell

---
+++ NOP.py
@@ -0,0 +1,35 @@
+import socket
+import threading
+from bind import execute
+class NOP:
+
+	def __init__(self, args):
+		self.args = args;
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		
+
+	def run(self):
+		if self.args.listen:
+			self.listen()
+		else:
+			self.connect()
+
+
+	def listen(self):
+
+		self.socket.bind(("localhost", self.args.port))
+		self.socket.listen(5)
+		while True:
+			client, addr = self.socket.accept()
+			m_thread = threading.Thread(target=self.handle_client, args=(client,))
+			m_thread.start()
+
+	def handle_client(self, client):
+		while True:
+			client.sendall(b"-->")
+			command_buffer = client.recv(128)
+			response = execute(command_buffer.decode())
+			if response:
+				client.sendall(response)
+			command_buffer = b""
--- bind.py
+++ bind.py
@@ -1,41 +1,49 @@
-import shlex
+import select
 import argparse
 import sys
+import NOP
 import subprocess
 def init_argparse():
 
 	parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
-				description='Simple implementation of netcat in python',
+				description='Simple Bind Shell in python',
 				epilog='''https://github.com/barishaxxer
 usage:\n 
-connect to somewhere:
-nc [options] hostname port\n
-reverse shell: 
-nc hostname port -e [shell]''')
+bind shell: 
+bind -l -p 1337''')
 	parser.add_argument('-l', '--listen', action='store_true', required=False)
 	parser.add_argument('-e', '--exec', required=False)
+	parser.add_argument('-p', '--port', type=int)
+	parser.add_argument('-i', '--ip')
 	return parser.parse_args()
 
 def is_windows():
 	return hasattr(sys, 'getwindowsversion')
 def execute(command):
+	output = "\n"
 	shell_type = ['/bin/bash', '-i']
 	if is_windows():
 		shell_type = [r'C:\Windows\System32\cmd.exe']
-		
 	interactive_shell = subprocess.Popen(shell_type, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	
 	if command == "exit":
-		sys.exit("Bye")
+		sys.exit("May the force be with you")
 	if command:
-		output = interactive_shell.communicate(input=command.encode())
-		print(output[0])
+		output = interactive_shell.communicate(input=command.encode())[0]
+	return output
+		
 			
 
 def main():
 
+	
+	shell_type = ['/bin/bash', '-i']
+	if is_windows():
+        	shell_type = [r'C:\Windows\System32\cmd.exe']
+
+	interactive_shell = subprocess.Popen(shell_type, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	args = init_argparse()
-	execute("cd ~/Documents/python_networking")
-	execute("pwd")
+	
+	nc = NOP.NOP(args)
+	nc.run()
 if __name__ == '__main__':
 	main()
